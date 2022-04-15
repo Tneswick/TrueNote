2152441c8e3d08a9b6129da0bb84e85aa0739329
Deck Stream <tneswick2@verizon.net>
2022-04-14 19:35:37 -0700

    readFile api GET

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,26 +2,24 @@ const router = require('express').Router()
 const notes = require('../../db/db.json')
 const { v4: uuidv4 } = require('uuid')
 const { createNewNote, deleteNote } = require('../../lib/notes')
+const fs = require('fs')
 
 router.get('/notes', (req, res) => {
-    res.json(notes)
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    return res.status(200).json(JSON.parse(data))
+  })
 })
 
 router.post('/notes', (req, res) => {
-    req.body.id = uuidv4();
-    const note = createNewNote(req.body)
-    res.json(note)
+  req.body.id = uuidv4()
+  const note = createNewNote(req.body)
+  return res.status(200).json(note)
 })
 
 router.delete('/notes/:id', (req, res) => {
-    // console.log(req.params.id);
-    const noteId = req.params.id;
-    // console.log(noteId);
-    // console.log(req.body);
-    // console.log(req.body.id);
-
-    deleteNote(noteId)
-    res.send('Delete Request Received')
+  const noteId = req.params.id
+  deleteNote(noteId)
+  res.send('Delete Request Received')
 })
 
-module.exports = router;
+module.exports = router
